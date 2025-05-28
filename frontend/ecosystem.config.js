22f223495f0f5788b17618f72f65f4940c58756f
Sergey Mironov <sergeymironov@protonmail.com>
2025-05-29 00:06:57 +0300

    Добавлен деплой фронтенда

---
+++ frontend/ecosystem.config.js
@@ -0,0 +1,18 @@
+const dotenv = require("dotenv");
+dotenv.config({ path: "./.env.deploy" });
+
+const { DEPLOY_USER, DEPLOY_HOST, DEPLOY_REF, DEPLOY_REPO, DEPLOY_PATH } =
+  process.env;
+
+module.exports = {
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: DEPLOY_REPO,
+      path: DEPLOY_PATH,
+      "post-deploy": "cd frontend && pwd && npm ci && npm run build",
+    },
+  },
+};
--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -24,7 +24,7 @@ function App() {
   const [selectedCard, setSelectedCard] = React.useState(null);
   const [cards, setCards] = React.useState([]);
 
-  
+
   const [currentUser, setCurrentUser] = React.useState({});
 
   const [isInfoToolTipOpen, setIsInfoToolTipOpen] = React.useState(false);
@@ -36,8 +36,9 @@ function App() {
   const history = useHistory();
 
 
+  const token = localStorage.getItem("jwt");
+
   React.useEffect(() => {
-    const token = localStorage.getItem("jwt");
     if (token) {
       api
         .checkToken(token)
@@ -58,10 +59,10 @@ function App() {
           console.log(err);
         });
     }
-  }, [history]);
+  }, [history, token]);
 
 
-  
+
   function handleEditProfileClick() {
     setIsEditProfilePopupOpen(true);
   }
@@ -166,15 +167,15 @@ function App() {
   }
 
   function onSignOut() {
-    
+
     localStorage.removeItem("jwt");
     setIsLoggedIn(false);
-    
+
     history.push("/signin");
   }
 
   return (
-    
+
     <CurrentUserContext.Provider value={currentUser}>
       <div className="page__content">
         <Header email={email} onSignOut={onSignOut} />
